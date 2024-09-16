5bc1daba9c4ba10a53554ee4630ddc5634344baa
Grace <hhchuang@pks.mpg.de>
2024-09-16 23:04:39 +0200

    Update toAnkiCard.sh

--- toAnkiCard.sh
+++ toAnkiCard.sh
@@ -1,50 +1,65 @@
 #!/bin/bash
-# 2024/02/17, Grace
 # Extract the information from each field from a table to a Anki card formate.
 # The number of field is user defined. Two key words are used 
 # 1. "## Vocabulary list": define the head line of table, which is 4 lines below
 # 2. "## Anki card": define the bottom of table, which is 2 lines above. 
 # 
+# 2024/02/17, Grace
+# 2024/08/03, Grace, Free the position of section of Vocabulary list and Anki card
 
 input=$1
 
-headKey="## Vocabulary list"
-tailKey="## Anki card"
+# case insensitive
+inputKey="Vocabulary list"
+outputKey="Anki card"
+cardType='Basic'
 
-line_s=$(grep -n -A 4 "$headKey" $input| tail -n 1 |awk '{print $1}'| tr -cd '[:digit:]. ')
-line_f=$(grep -n -B 2 "$tailKey" $input| head -n 1 |awk '{print $1}'| tr -cd '[:digit:]. ')
+targetDeck=$(grep "TARGET DECK" $input|head -n 1)
 
-num_card=$(($line_f - $line_s -1))
+# Calculate the initial and final number of line for the input vocabulary list, and also the total number of vocabularies. 
+vocab_iniL=$(grep -n "##" $input | grep -i "$inputKey"| cut -d ':' -f 1| head -n 1 ) 
+vocab_finL=$(grep -n "##" $input | grep -A 1 -i "$inputKey"| cut -d ':' -f 1| tail -n 1 ) 
+temp_iniL=$(sed -n "$vocab_iniL,$vocab_finL p" $input | grep -n '|'| cut -d ':' -f 1 |head -n 1)
+temp_finL=$(sed -n "$vocab_iniL,$vocab_finL p" $input | grep -n '|'| cut -d ':' -f 1 |tail -n 1)
+nVocab=$(($temp_finL - $temp_iniL - 1))
+vocab_iniL=$(($vocab_iniL + $temp_iniL - 1))
+vocab_finL=$(($vocab_iniL + $nVocab + 1))
 
-field=($(sed -n "$line_s,$line_s p" $input | sed 's/|//g'))
+field=($(sed -n "$vocab_iniL,$vocab_iniL p" $input | sed 's/|//g'))
 num_field=${#field[@]}
 
-line_s_card=$(($line_s + 2))
-# cannot use array because of blank in elements.
-for ((n=1;n<=$num_field;n++))
+# Calculate the output line number 
+output_iniL=$(grep -n "##" $input | grep -i "$outputKey"| cut -d ':' -f 1| head -n 1 )
+
+# extract metadata into $vocab_field[$m,$n]
+
+# use 2d array variable 
+declare -a vocab_field
+
+output_iniL=$(($output_iniL+1))
+sed -i '' "$output_iniL s/^/$targetDeck\n/" $input 
+sed -i '' "$output_iniL s/^/ \n/" $input 
+output_iniL=$(($output_iniL+2))
+
+for ((n=1;n<=$nVocab;n++))
 do 
-    sed -n "$line_s_card,$line_f p" $input | cut -d '|' -f $(($n+1)) > tmp_toAnki$n.dat  
-done 
-
-#Main program 
-
-grep -A 2 "$headKey"  $input | tail -n 1 >> $input 
-echo '' >> $input 
-
-for ((n=1;n<=$num_card;n++))
-do 
-    echo 'START' >> $input 
-    echo 'DU1 (DU to EN)' >> $input 
+    echo 'START' > card.tmp 
+    echo "$cardType" >> card.tmp 
+    
+    tmp_Line=$(($vocab_iniL+1+$n))
 
     for  ((m=1;m<=$num_field;m++))
     do 
-        col=$(sed -n "$n,$n p" tmp_toAnki$m.dat) 
-        echo ${field[$(($m-1))]}':' "$col" >> $input 
+        vocab_field[$m,$n]=$(sed -n "$tmp_Line,$tmp_Line p" $input | cut -d '|' -f $(($m+1)))
+        echo ${field[$(($m-1))]}: ${vocab_field[$m,$n]} >> card.tmp 
     done 
-    
-    echo '' >> $input 
-    echo 'END' >> $input 
-    echo '' >> $input 
+    echo '' >> card.tmp 
+    echo 'END' >> card.tmp 
+    echo '' >> card.tmp 
+
+    sed -i '' "$output_iniL r card.tmp" $input
+    output_iniL=$(($output_iniL+$num_field+5))
+
 done 
 
-rm -f tmp_toAnki*.dat
+rm -f card.tmp 
